15079cc8ce5dae653781351134b9a3ab3c761dc5
Gundavri <m.mirianashvili99@gmail.com>
2020-01-09 03:02:05 +0400

    body-parser, morgan, cors and other stuff added

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+// Routes which handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        error: {
+            message: err.message
+        }
+    });
+});
 
 module.exports = app;
