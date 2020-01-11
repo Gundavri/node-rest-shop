82afc3c0d43dbf80c83c5b4b3f13657adc05e6a3
Gundavri <m.mirianashvili99@gmail.com>
2020-01-12 03:00:07 +0400

    fetching data from ProductsDB when using 'GET' on orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -7,7 +7,7 @@ const Product = require('../models/product');
 
 // Handle incoming GET requests to /orders
 router.get('/', (req, res, next) => {
-    Order.find().select('product quantity _id').then(docs => {
+    Order.find().select('product quantity _id').populate('product', 'name').then(docs => {
         res.status(200).json({
             count: docs.length,
             orders: docs.map(doc => {
@@ -67,7 +67,7 @@ router.post('/', (req, res, next) => {
 });
 
 router.get('/:orderId', (req, res, next) => {
-    Order.findById(req.params.orderId).then(order => {
+    Order.findById(req.params.orderId).populate('product').then(order => {
         if(!order) {
             return res.status(404).json({
                 message: 'Order not found'
