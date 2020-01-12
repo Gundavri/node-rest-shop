fd13488747458f2a4ebfb23f6f8764cf85b19be9
Gundavri <m.mirianashvili99@gmail.com>
2020-01-13 03:30:21 +0400

    User signup and delete added. bcrypt package is installed

--- app.js
+++ app.js
@@ -7,13 +7,15 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect('mongodb+srv://Gundavri:' 
                     + process.env.MONGO_ATLAS_PW 
                         + '@node-rest-shop-2xspm.mongodb.net/test?retryWrites=true&w=majority',
                         {
                             useNewUrlParser: true,
-                            useUnifiedTopology: true
+                            useUnifiedTopology: true,
+                            useCreateIndex: true
                         });
 
 // Deprecation warning
@@ -33,6 +35,7 @@ app.use(cors());
 // Routes which handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
