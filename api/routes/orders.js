d36cb1ba47221fe6288a9c11a170b1b78230363f
Gundavri <m.mirianashvili99@gmail.com>
2020-01-13 14:04:03 +0400

    JWT route protection added

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const checkAuth = require('../middleware/check-auth');
 
 const Order = require('../models/order');
 const Product = require('../models/product');
 
 // Handle incoming GET requests to /orders
-router.get('/', (req, res, next) => {
+router.get('/', checkAuth, (req, res, next) => {
     Order.find().select('product quantity _id').populate('product', 'name').then(docs => {
         res.status(200).json({
             count: docs.length,
@@ -27,7 +28,7 @@ router.get('/', (req, res, next) => {
     });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', checkAuth, (req, res, next) => {
     Product.findById(req.body.productId).then(product => {
         if(!product){
             return res.status(404).json({
@@ -66,7 +67,7 @@ router.post('/', (req, res, next) => {
     });
 });
 
-router.get('/:orderId', (req, res, next) => {
+router.get('/:orderId', checkAuth, (req, res, next) => {
     Order.findById(req.params.orderId).populate('product').then(order => {
         if(!order) {
             return res.status(404).json({
@@ -85,7 +86,7 @@ router.get('/:orderId', (req, res, next) => {
     });
 });
 
-router.delete('/:orderId', (req, res, next) => {
+router.delete('/:orderId', checkAuth, (req, res, next) => {
     Order.deleteOne({_id: req.params.orderId}).then(result => {
         res.status(200).json({
             message: 'Order deleted',
